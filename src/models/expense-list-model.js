e783b1b26239af0769d8d1162f73747bfef0bbc2
nelson <nnamusoboko@gmail.com>
2025-10-26 23:59:34 +0300

    feat: add expense-list sample object

---
+++ src/models/expense-list-model.js
@@ -0,0 +1,13 @@
+const arr = [
+  { id: 1, 
+    name: 'Hosting Website', 
+    amount: 300, 
+    category: 'Cloud', 
+    date: new Date().getDate.toString() 
+  }, { id: 2, 
+    name: 'New Computer', 
+    amount: 300, 
+    category: 'Electronics', 
+    date: new Date().getDate.toString() 
+  }
+];
