64e2c32aefbb512519b1335b847858800481f06e
nelson <nnamusoboko@gmail.com>
2025-11-02 03:01:33 +0300

    feat(controller): implement function to return expenses count

--- src/controllers/expense-list-contoller.js
+++ src/controllers/expense-list-contoller.js
@@ -1,7 +1,11 @@
-import { getExpenses } from '../models/expense-list-model.js';
+import { getExpenses, getStoredExpensesCount } from '../models/expense-list-model.js';
 import { renderExpenseList } from '../views/expense-list-view.js';
 
 export function renderExpenses(expenseCount=0) {
   const expenses = getExpenses(expenseCount);
   return renderExpenseList(expenses);
+}
+
+export function expenseCount() {
+  return getStoredExpensesCount();
 }
