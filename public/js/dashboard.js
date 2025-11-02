f3baae2683497a2e4e36f9983c872bfa603b339b
nelson <nnamusoboko@gmail.com>
2025-11-03 00:10:03 +0300

    refactor: remove redundant code

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,6 @@
 import { expenseCount, renderExpenses} from '../../src/controllers/expense-list-contoller.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const storedExpensesCount = expenseCount();
-  console.log("Stored expenses number: ", storedExpensesCount);
-
-  
   const  overviewBtn = document.querySelector('.js-overview-btn');
   const expensesList = document.querySelector('.js-expenses-list');
 
